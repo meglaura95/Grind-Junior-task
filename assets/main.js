dcc3518c6b0d780be5932f93de76772535e326cf
meglaur95 <meglaura95>
2023-11-27 14:21:42 +0000

    Connected Jsonplaceholder API

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,49 @@
 const selectors = {
-  peopleButton: document.querySelector('[search-button]'),
-}
+  peopleButton: document.querySelector('.form__btn'),
+  userIdInput: document.querySelector('.form__input'),
+  userDetailsDiv: document.querySelector('.search-results'),
+};
 
 const setClickEvents = () => {
   selectors.peopleButton.addEventListener('click', () => {
-    console.log('Button clicked!')
-    
-    // Think fetch...
-  })
-}
+    const userId = selectors.userIdInput.value;
+
+    if (!userId) {
+      alert('Please enter a user ID');
+      return;
+    }
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('User not found');
+        }
+        return response.json();
+      })
+      .then(user => {
+        displayUserDetails(user);
+      })
+      .catch(error => {
+        console.error('Error fetching user:', error);
+        alert('User not found. Please enter a valid ID.');
+      });
+  });
+};
+
+const displayUserDetails = user => {
+  selectors.userDetailsDiv.innerHTML = `
+    <div class="user-card">
+   
+<img src="https://i.pravatar.cc/300" alt="User Avatar">
+<ul><li><h2>${user.name}</h2><li>
+      <li><p><strong> ${user.email}</strong></p></li>
+      <li><p><strong> ${user.phone}</strong></p></li></ul>
+    </div>
+  `;
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello!')
+  console.log('Hello!');
 
-  setClickEvents()
-})
+  setClickEvents();
+});
